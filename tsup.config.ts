c0583bd3e325e53c799684e742ae5cbdf1ad3847
bitomic <35199700+bitomic@users.noreply.github.com>
2023-03-04 19:31:10 -0700

    chore: fix build options

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig( {
 	clean: true,
 	dts: true,
-	entry: [ 'src/main.ts' ],
+	entry: [ 'src/main.ts', 'src/tests/Controllers.ts' ],
 	format: [ 'cjs', 'esm' ],
 	minify: true,
 	sourcemap: true,
